ed458fd8d53eaa67c2fea8b19c8fd6a2ab00729b
ubitinonevami <bernastya@inbox.ru>
2020-09-08 21:48:45 +0300

    Оформлена главная стрвницв

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,3 @@
-
-//open main-nav
-
 var mainnav = document.querySelector('.mobile-popup');
 var openButton = document.querySelector('.main-nav__toggle');
 
@@ -23,7 +20,6 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-//fix-header
 var header = document.querySelector(".page-header");
 var headerCont = document.querySelector('.page-header__container');
 var topOfHeader = header.offsetTop;
@@ -36,10 +32,127 @@ function fixHeader() {
 }
 window.addEventListener("scroll", fixHeader);
 
-//modal popup bussiness-offer
+var list = document.querySelector('.country-filter-popup__list');
+var wrapper = document.querySelector('.country-filter-popup__alphabet-wrapper');
+var openButton = document.querySelector('.country-filter__toggle');
+var closeWrapButton = document.querySelector('.country-filter-popup__close-btn');
+
+openButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (wrapper.classList.contains('country-filter-popup__alphabet-wrapper--closed')) {
+    wrapper.classList.remove('country-filter-popup__alphabet-wrapper--closed');
+    wrapper.classList.add('country-filter-popup__alphabet-wrapper--open');
+    list.classList.remove('country-filter-popup__list--closed');
+  } else {
+    wrapper.classList.add('country-filter-popup__alphabet-wrapper--closed');
+    wrapper.classList.remove('country-filter-popup__alphabet-wrapper--open');
+    list.classList.add('country-filter-popup__list--closed');
+  }
+});
+
+closeWrapButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  wrapper.classList.remove('country-filter-popup__alphabet-wrapper--open');
+  wrapper.classList.add('country-filter-popup__alphabet-wrapper--closed');
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (wrapper.classList.contains("country-filter-popup__alphabet-wrapper--open")) {
+      wrapper.classList.remove("country-filter-popup__alphabet-wrapper--open");
+    }
+  }
+});
+//accordeon
+
+var filterItem = document.querySelectorAll('.traveller-filter-list__item').forEach(item =>
+  item.addEventListener('click', () => {
+    var options = document.querySelectorAll('.traveller-filter__option').forEach(option => {
+      if (option.classList.contains('traveller-filter__option--active')) {
+        option.classList.remove('traveller-filter__option--active');
+      } else {
+        option.classList.toggle('traveller-filter__option--open');
+      }
+    })
+  }))
+
+
+
+const alphabetWrapper = document.querySelectorAll('.alphabet__wrapper');
+alphabetWrapper.forEach(wrapper => {
+  wrapper.addEventListener('click', function () {
+    if (wrapper.classList.contains('alphabet__wrapper--current')) {
+      wrapper.classList.remove('alphabet__wrapper--current');
+    } else {
+      wrapper.classList.add('alphabet__wrapper--current');
+    }
+
+  })
+})
+
+
+
+//counter
+
+const btns = document.querySelectorAll('.plan-filters__toggle');
+btns.forEach(btn => {
+  btn.addEventListener('click', function () {
+    const direction = this.dataset.direction;
+    const input = this.parentElement.querySelector('.plan-filters__input');
+    const currentValue = +input.value;
+    let newValue;
+
+    if (direction === 'plus') {
+      newValue = currentValue + 1;
+    } else {
+      newValue = currentValue - 1 > 0 ?
+        currentValue - 1 : 0;
+    }
+
+    input.value = newValue;
+  })
+})
+
+
+//calendar popup
+var buttonChoose = document.querySelector('.plan-filters__button--choose');
+var addPlanModal = document.querySelector('.add-plan-popup');
+var closeButton = document.querySelector('.add-plan-popup__button-close');
+
+buttonChoose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  addPlanModal.classList.add('add-plan-popup--open');
+});
+
+closeButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  addPlanModal.classList.remove('add-plan-popup--open');
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (addPlanModal.classList.contains("add-plan-popup--open")) {
+      addPlanModal.classList.remove("add-plan-popup--open");
+    }
+  }
+});
+
+var form = document.querySelector(".plan-filters__comment");
+var textarea = document.querySelector("[name=comments]");
+var buttonSubmit = document.querySelector(".plan-filters__btn-submit");
+
+buttonSubmit.addEventListener("click", function (evt) {
+  if (!textarea.value) {
+    evt.preventDefault();
+    form.classList.add("plan-filters__comment--error");
+  } else {
+    form.classList.remove("plan-filters__comment--error");
+  }
+});
 
 var tariffes = document.querySelector('.tariffes');
-var closeButton = document.querySelector('.tariffes__button');
+var closeButtonTariffes = document.querySelector('.tariffes__button');
 var link = document.querySelector('.business-offer__link');
 
 
@@ -48,7 +161,7 @@ link.addEventListener('click', function (evt) {
   tariffes.classList.add('tariffes--open');
 });
 
-closeButton.addEventListener('click', function (evt) {
+closeButtonTariffes.addEventListener('click', function (evt) {
   evt.preventDefault();
   tariffes.classList.remove('tariffes--open');
 });
@@ -58,5 +171,18 @@ window.addEventListener("keydown", function (evt) {
     if (tariffes.classList.contains("tariffes--open")) {
       tariffes.classList.remove("tariffes--open");
     }
+  }
+});
+
+var emailForm = document.querySelector(".promotion__form-field");
+var emailValue = document.querySelector("[name=email]");
+var buttonPromotion = document.querySelector(".promotion__button");
+
+buttonPromotion.addEventListener("click", function (evt) {
+  if (!emailValue.value) {
+    evt.preventDefault();
+    emailForm.classList.add("promotion__form-field--error");
+  } else {
+    emailForm.classList.remove("promotion__form-field--error");
   }
 });
